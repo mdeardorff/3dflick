62a324a72ecb3eb39cd2192dae8bb68efe713dec
mdeardorff <mdeardorff123@gmail.com>
2017-02-27 12:26:16 -0600

    More stuff

---
+++ 3dflick/Assets/discScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class discScript : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float force;
+
+	// Use this for initialization
+	void Start () {
+		force = 10.0F;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			rb.AddForce (transform.right * force, ForceMode.Impulse);
+		}
+	}
+}
