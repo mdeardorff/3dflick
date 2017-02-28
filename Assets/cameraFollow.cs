329d0501e8f4af78c2087a7564a154330792650d
mdeardorff <mdeardorff123@gmail.com>
2017-02-27 23:01:00 -0600

    Attempting to implement lerping between camera targets

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector3 offset;
+	public GameObject target;
+
+
+	void Start () {
+		offset.x = -10.0F;
+		offset.y = 7.0F;
+		offset.z = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = target.transform.position + offset;
+	}
+
+	Vector3 getOffset() {
+		return offset;
+	}
+		
+}
