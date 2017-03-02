d1f02150fed3441d48214f27e4b8376d5e33a332
Nate Mize <Nate Mize>
2017-03-01 21:29:42 -0600

    Pause Menu Functionality

---
+++ Assets/Scripts/pauseScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauseScript : MonoBehaviour {
+
+	public Transform canvas;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		pause ();
+	}
+
+	public void pause() {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (canvas.gameObject.activeInHierarchy == false) {
+				canvas.gameObject.SetActive (true);
+				Time.timeScale = 0;
+				AudioListener.volume = 0;
+			} else {
+				canvas.gameObject.SetActive (false);
+				Time.timeScale = 1;
+				AudioListener.volume = 1;
+			}
+		}
+	}
+}
+
