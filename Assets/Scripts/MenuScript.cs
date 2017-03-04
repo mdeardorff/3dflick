f62610bc4cce524c55f51f697ff92221000d3296
Nate Mize <Nate Mize>
2017-03-03 23:51:32 -0600

    Added Title Menu Scene#

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas MainCanvas;
+	public Canvas OptionsCanvas;
+	public Button SoundButton;
+
+	void Awake()
+	{
+		OptionsCanvas.enabled = false;
+		SoundButton.GetComponent<Image> ().color = Color.green;
+	}
+
+	public void OptionsOn()
+	{
+		OptionsCanvas.enabled = true;
+		MainCanvas.enabled = false;
+	}
+
+	public void ReturnToMain()
+	{
+		OptionsCanvas.enabled = false;
+		MainCanvas.enabled = true;
+	}
+
+	public void LoadIntoGame()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void SoundToggle()
+	{
+		if (AudioListener.volume == 0) {
+			SoundButton.GetComponent<Image> ().color = Color.green;
+			AudioListener.volume = 1;
+		} else {
+			SoundButton.GetComponent<Image> ().color = Color.red;
+			AudioListener.volume = 0;
+		}
+	}
+}
